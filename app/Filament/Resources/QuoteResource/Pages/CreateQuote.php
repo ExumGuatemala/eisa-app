c06568128bf299149f5e8bf87d8d0e04b2cea727
Kenny Barrera <barrerakj@gmail.com>
2024-01-26 18:27:06 -0600

    Agregar notificacion

--- app/Filament/Resources/QuoteResource/Pages/CreateQuote.php
+++ app/Filament/Resources/QuoteResource/Pages/CreateQuote.php
@@ -3,13 +3,31 @@
 namespace App\Filament\Resources\QuoteResource\Pages;
 
 use App\Filament\Resources\QuoteResource;
+use App\Models\Client;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use App\Services\QuoteService;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 
 class CreateQuote extends CreateRecord
 {
     protected static string $resource = QuoteResource::class;
+    
+    protected function beforeCreate(): void
+    {
+        $client = Client::where("id", $this->data["clientId"])->first();
+        if ($client->state == "Deshabilitado") {
+            Notification::make()
+                ->warning()
+                ->title('Cliente Deshabilitado')
+                ->body('El cliente que ha elegido esta deshabilitado. Por favor elija otro.')
+                ->persistent()
+                ->send();
+        
+            $this->halt();
+        }
+    }
     
     protected function afterCreate(): void
     {
